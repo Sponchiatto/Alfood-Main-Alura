7ff3c21c219b87278e7956f5980a728471de6e9e
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-07-31 14:28:20 -0300

    Layout Base Pagina Admin Restaurantes

---
+++ src/paginas/Administracao/PaginaBaseAdmin.tsx
@@ -0,0 +1,46 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Container,
+  Toolbar,
+  Link,
+  Paper,
+} from "@mui/material";
+import { Outlet, Link as RouterLink } from "react-router-dom";
+
+const PaginaBaseAdmin = () => {
+  return (
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit">
+              Administração
+            </Typography>
+            <Box sx={{ display: "flex", flexGrow: 1 }}>
+              <Link component={RouterLink} to={"/admin/restaurantes"}>
+                <Button sx={{ my: 2, color: "white" }}>Restaurantes</Button>
+              </Link>
+              <Link component={RouterLink} to={"/admin/restaurantes/novo"}>
+                <Button sx={{ my: 2, color: "white" }}>Novo Restaurante</Button>
+              </Link>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 1 }}>
+          <Paper sx={{ p: 2 }}>
+            <Outlet />
+          </Paper>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default PaginaBaseAdmin;
--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,9 +1,20 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Container,
+  Toolbar,
+  Link,
+  Paper,
+} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import http from "../../../http";
+import { Link as RouterLink } from "react-router-dom";
 
 const FormularioRestaurante = () => {
   const parametros = useParams();
@@ -43,32 +54,49 @@ const FormularioRestaurante = () => {
     }
   };
   return (
-    <Box
-      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <Typography component="h1" variant="h6">
-        Formulário de Restaurantes
-      </Typography>
-      <Box component="form" onSubmit={aoSubmeterForm}>
-        <TextField
-          value={nomeRestaurante}
-          onChange={(evento) => setNomeRestaurante(evento.target.value)}
-          id="standard-basic"
-          label="Nome do Restaurante"
-          variant="standard"
-          fullWidth
-          required
-        />
-        <Button
-          sx={{ marginTop: 1 }}
-          type="submit"
-          variant="outlined"
-          fullWidth
-        >
-          Salvar
-        </Button>
+    <>
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 1 }}>
+          <Paper sx={{ p: 2 }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                flexGrow: 1,
+              }}
+            >
+              <Typography component="h1" variant="h6">
+                Formulário de Restaurantes
+              </Typography>
+              <Box
+                component="form"
+                sx={{ width: "100%" }}
+                onSubmit={aoSubmeterForm}
+              >
+                <TextField
+                  value={nomeRestaurante}
+                  onChange={(evento) => setNomeRestaurante(evento.target.value)}
+                  id="standard-basic"
+                  label="Nome do Restaurante"
+                  variant="standard"
+                  fullWidth
+                  required
+                />
+                <Button
+                  sx={{ marginTop: 1 }}
+                  type="submit"
+                  variant="outlined"
+                  fullWidth
+                >
+                  Salvar
+                </Button>
+              </Box>
+            </Box>
+          </Paper>
+        </Container>
       </Box>
-    </Box>
+    </>
   );
 };
 
